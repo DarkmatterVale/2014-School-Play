9ddfd51ccdf01c6c242474c292964dae66d68b5f
Vale <valetolpegin@gmail.com>
2014-10-12 08:57:47 -0400

    added code

--- ActivityBot.c
+++ ActivityBot.c
@@ -1,1 +1,36 @@
-//Add ActivityBot code here
+//ActivityBot code here
+
+#include "simpletools.h"                      // Include simple tools
+#include "fdserial.h"
+#include "abdrive.h"
+
+fdserial *xbee;
+
+int main()                                    // Main function
+{
+  xbee = fdserial_open( 9, 8, 0, 9600 );
+
+  char c;
+
+  while ( 1 )
+  {
+    c = fdserial_rxChar( xbee );
+ 
+    if ( c == 'f' )
+    {
+      drive_speed( 64, 64 );
+    } else if ( c == 'b' )
+    {
+      drive_speed( -64, -64 );
+    } else if ( c == 'l' )
+    {
+      drive_speed( 0, 64 );
+    } else if ( c == 'r' )
+    {
+      drive_speed( 64, 0 );
+    } else if ( c == 's' )
+    {
+      drive_speed( 0, 0 );
+    }
+  }
+}
