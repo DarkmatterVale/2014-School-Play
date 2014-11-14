61efd6ec6cbc6a68382c1d25f5795ea8b65761f2
Vale <valetolpegin@gmail.com>
2014-11-14 15:24:01 -0500

    Made speed faster

--- ActivityBot.c
+++ ActivityBot.c
@@ -18,16 +18,16 @@ int main()                                    // Main function
     {
       if ( data == 'f' ) //If the data incoming is telling the robot to move forward
       {
-        drive_speed( 64, 64 ); //move forward at 1/2 speed
+        drive_speed( 128, 128 ); //move forward at 1/2 speed
       } else if ( data == 'b' ) //If the data incoming is telling the robot to move backward
       {
-        drive_speed( -64, -64 ); //move backward at 1/2 speed
+        drive_speed( -128, -128 ); //move backward at 1/2 speed
       } else if ( data == 'l' ) //If the data incoming is telling the root to turn left
       {
-        drive_speed( 0, 64 ); //turn left in a spin turn at 1/2 speed
+        drive_speed( 0, 128 ); //turn left in a spin turn at 1/2 speed
       } else if ( data == 'r' ) //If the data incoming is telling the robot to turn right
       {
-        drive_speed( 64, 0 ); //turn right in a spin turn at 1/2 speed
+        drive_speed( 128, 0 ); //turn right in a spin turn at 1/2 speed
       } else if ( data == 's' ) //If the data incoming is telling the robot to stop
       {
         drive_speed( 0, 0 ); //stop
